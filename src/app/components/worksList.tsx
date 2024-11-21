20b5ab891e4f8eeb5395f9aa0bdce8d9d6c9a968
rintm <41986326+rit3919@users.noreply.github.com>
2024-11-21 15:35:29 +0900

    add preskate article

--- src/app/components/worksList.tsx
+++ src/app/components/worksList.tsx
@@ -5,23 +5,170 @@ const works = [
         name: 'PreSkate -仮想スケート体験システム-',
         body: (
             <Box>
-                <Typography variant="body1" sx={{ padding: '0 0 2vh 0' }}>
-                    技術:
-                </Typography>
-                <Typography variant="body1" sx={{ padding: '0 0 2vh 0' }}>
-                    このシステムでは，Unityを用いて作成しました．
-                </Typography>
-                <Typography variant="body1" sx={{ padding: '0 0 2vh 0' }}>
-                    また，スケートをする際の動きを検知するために，Kinectを使用しています．
-                </Typography>
-                <Typography variant="body1" sx={{ padding: '0 0 2vh 0' }}>
-                    このシステムは，スケートをしたことがない人でも，スケートを体験できるように作成しました．
-                </Typography>
+                <Box sx={{ padding: '0 0 1vh 0' }}>
+                    <Typography variant='h6' >
+                        概要:
+                    </Typography>
+                    <Typography sx={{ padding: '0 0 0 2vw' }}>
+                        高専時代に参加した第32回高専プログラミングコンテスト課題部門での作品です。HMDをかぶり、スケートを体験できるプロダクトです。コンテストの本戦に進むことができ、敢闘賞を受賞することができました。
+                    </Typography>
+                </Box>
+                <Box sx={{ padding: '0 0 1vh 0' }}>
+                    <Typography variant='h6' >
+                        期間:
+                    </Typography>
+                    <Typography sx={{ padding: '0 0 0 2vw' }}>
+                        5ヶ月
+                    </Typography>
+                </Box>
+                <Box sx={{ padding: '0 0 1vh 0' }}>
+                    <Typography variant='h6' >
+                        開発環境:
+                    </Typography>
+                    <Typography sx={{ padding: '0 0 0 2vw' }}>
+                        C#,unity,javascript,Git
+                    </Typography>
+                </Box>
+                <Box sx={{ padding: '0 0 1vh 0' }}>
+                    <Typography variant='h6' >
+                        人数、自分の役割:
+                    </Typography>
+                    <Typography sx={{ padding: '0 0 0 2vw' }}>
+                        5人、プロジェクトリーダーとバックエンドを努めました。
+                    </Typography>
+                </Box>
+                <Box sx={{ padding: '0 0 1vh 0' }}>
+                    <Typography variant='h6' >
+                        学び・得たこと:
+                    </Typography>
+                    <Typography sx={{ padding: '0 0 0 2vw' }}>
+                        プロジェクトリーダーとして、複数人で同じプロジェクトを管理する方法を学ぶことができました。開発については、研究室内で管理するプロジェクトだったため、GitHubを使うことができず、オンプレミス環境でのリモートリポジトリ環境の構築を学ぶことができました。
+                    </Typography>
+                </Box>
+
+                <Box sx={{ padding: '0 0 1vh 0' }}>
+                    <Typography variant='h6' >
+                        困難だったこと:
+                    </Typography>
+                    <Typography sx={{ padding: '0 0 0 2vw' }}>
+                        進捗をうまく管理することができず、プログラミングが苦手な人の進捗を管理することが大変でした。解決方法としては、マネジメントの一部を変わってもらい、開発系を私が専念することにしました。
+                    </Typography>
+                </Box>
             </Box>
         )
     },
-    { name: 'LocationDraw', level: 'UnityC#として使用', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg' },
-    { name: 'Regeno', level: 'このポートフォリオの作成に使用', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg' }
+    {
+        name: 'LocationDraw',
+        body: (
+            <Box>
+                <Box sx={{ padding: '0 0 1vh 0' }}>
+                    <Typography variant='h6' >
+                        概要:
+                    </Typography>
+                    <Typography sx={{ padding: '0 0 0 2vw' }}>
+                        大学3年次の授業、システム創生プロジェクトで取り組んだプロジェクトです。歩いた道のりを地図に自動で書いて絵を描くことをコンセプトに作成したプロジェクトでした。
+                    </Typography>
+                </Box>
+                <Box sx={{ padding: '0 0 1vh 0' }}>
+                    <Typography variant='h6' >
+                        期間:
+                    </Typography>
+                    <Typography sx={{ padding: '0 0 0 2vw' }}>
+                        2ヶ月
+                    </Typography>
+                </Box>
+                <Box sx={{ padding: '0 0 1vh 0' }}>
+                    <Typography variant='h6' >
+                        開発環境:
+                    </Typography>
+                    <Typography sx={{ padding: '0 0 0 2vw' }}>
+                        Ruby,Rails,javascript
+                    </Typography>
+                </Box>
+                <Box sx={{ padding: '0 0 1vh 0' }}>
+                    <Typography variant='h6' >
+                        人数、自分の役割:
+                    </Typography>
+                    <Typography sx={{ padding: '0 0 0 2vw' }}>
+                        10人、GPSから地図を書くという機能の根幹の開発を行いました。
+                    </Typography>
+                </Box>
+                <Box sx={{ padding: '0 0 1vh 0' }}>
+                    <Typography variant='h6' >
+                        学び・得たこと:
+                    </Typography>
+                    <Typography sx={{ padding: '0 0 0 2vw' }}>
+                        WebAPIなどのwebアプリ開発に関する知識を知ることができました。
+                    </Typography>
+                </Box>
+
+                <Box sx={{ padding: '0 0 1vh 0' }}>
+                    <Typography variant='h6' >
+                        困難だったこと:
+                    </Typography>
+                    <Typography sx={{ padding: '0 0 0 2vw' }}>
+                        デプロイをする際に、位置情報を使うアプリケーションを使うためにはhttpsである必要がありますそこのことに気づくまでに時間がかかってしまいました。デジタル証明書を発行することはできないので、デバッグモードでhttpで実行できるようにすることで解決しました。
+                    </Typography>
+                </Box>
+            </Box>
+        )
+    },
+    {
+        name: 'Regeno',
+        body: (
+            <Box>
+                <Box sx={{ padding: '0 0 1vh 0' }}>
+                    <Typography variant='h6' >
+                        概要:
+                    </Typography>
+                    <Typography sx={{ padding: '0 0 0 2vw' }}>
+
+                    </Typography>
+                </Box>
+                <Box sx={{ padding: '0 0 1vh 0' }}>
+                    <Typography variant='h6' >
+                        期間:
+                    </Typography>
+                    <Typography sx={{ padding: '0 0 0 2vw' }}>
+
+                    </Typography>
+                </Box>
+                <Box sx={{ padding: '0 0 1vh 0' }}>
+                    <Typography variant='h6' >
+                        開発環境:
+                    </Typography>
+                    <Typography sx={{ padding: '0 0 0 2vw' }}>
+
+                    </Typography>
+                </Box>
+                <Box sx={{ padding: '0 0 1vh 0' }}>
+                    <Typography variant='h6' >
+                        人数、自分の役割:
+                    </Typography>
+                    <Typography sx={{ padding: '0 0 0 2vw' }}>
+
+                    </Typography>
+                </Box>
+                <Box sx={{ padding: '0 0 1vh 0' }}>
+                    <Typography variant='h6' >
+                        学び・得たこと:
+                    </Typography>
+                    <Typography sx={{ padding: '0 0 0 2vw' }}>
+
+                    </Typography>
+                </Box>
+
+                <Box sx={{ padding: '0 0 1vh 0' }}>
+                    <Typography variant='h6' >
+                        困難だったこと:
+                    </Typography>
+                    <Typography sx={{ padding: '0 0 0 2vw' }}>
+
+                    </Typography>
+                </Box>
+            </Box>
+        )
+    }
 ];
 
 export default works;
