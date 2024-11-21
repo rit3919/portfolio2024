3560463932da5aef580f252fddff7317d771857a
rit3919 <41986326+rit3919@users.noreply.github.com>
2024-11-21 16:13:09 +0900

    add regino article

--- src/app/components/worksList.tsx
+++ src/app/components/worksList.tsx
@@ -1,4 +1,5 @@
 import { Box, Typography } from '@mui/material';
+import Image from 'next/image';
 
 const works = [
     {
@@ -107,7 +108,7 @@ const works = [
                         困難だったこと:
                     </Typography>
                     <Typography sx={{ padding: '0 0 0 2vw' }}>
-                        デプロイをする際に、位置情報を使うアプリケーションを使うためにはhttpsである必要がありますそこのことに気づくまでに時間がかかってしまいました。デジタル証明書を発行することはできないので、デバッグモードでhttpで実行できるようにすることで解決しました。
+                        デプロイをする際に、位置情報を使うアプリケーションを使うためにはhttpsである必要があります。そこに気づくまでに時間がかかってしまいました。デジタル証明書を発行することはできないので、デバッグモードでhttpで実行できるようにすることで解決しました。
                     </Typography>
                 </Box>
             </Box>
@@ -122,7 +123,7 @@ const works = [
                         概要:
                     </Typography>
                     <Typography sx={{ padding: '0 0 0 2vw' }}>
-
+                            大学4年次の授業、システム創生プロジェクトで取り組んだプロジェクトです。私の大学の履修登録や履修確認をわかりやすくできるようにするプロジェクトでした。
                     </Typography>
                 </Box>
                 <Box sx={{ padding: '0 0 1vh 0' }}>
@@ -130,15 +131,15 @@ const works = [
                         期間:
                     </Typography>
                     <Typography sx={{ padding: '0 0 0 2vw' }}>
-
+                        2ヶ月
                     </Typography>
                 </Box>
                 <Box sx={{ padding: '0 0 1vh 0' }}>
                     <Typography variant='h6' >
-                        開発環境:
+                        開発環境: 
                     </Typography>
                     <Typography sx={{ padding: '0 0 0 2vw' }}>
-
+                            Ruby,Rails,javascript,React,Git
                     </Typography>
                 </Box>
                 <Box sx={{ padding: '0 0 1vh 0' }}>
@@ -146,25 +147,27 @@ const works = [
                         人数、自分の役割:
                     </Typography>
                     <Typography sx={{ padding: '0 0 0 2vw' }}>
-
+                        9人、フロントエンドのマネジメント、チームのサブリーダーを努めました。
                     </Typography>
                 </Box>
-                <Box sx={{ padding: '0 0 1vh 0' }}>
+                <Typography variant='h5' sx={{padding:'2vh 0 1vh 0'}}>
+                    実装例
+                </Typography>
+                <Box sx={{ padding: '5vh 0 1vh 0' }}>
                     <Typography variant='h6' >
-                        学び・得たこと:
+                        画面例①：時間割表の切り替え
                     </Typography>
-                    <Typography sx={{ padding: '0 0 0 2vw' }}>
-
-                    </Typography>
+                    <Box>
+                        <Image src='/image/regeno/changeTable.gif' alt='changeTable' width={1000} height={300} style={{ pointerEvents: 'none', userSelect: 'none' }} />
+                    </Box>
                 </Box>
-
-                <Box sx={{ padding: '0 0 1vh 0' }}>
+                <Box sx={{ padding: '5vh 0 1vh 0' }}>
                     <Typography variant='h6' >
-                        困難だったこと:
+                        画面例②：履修登録画面
                     </Typography>
-                    <Typography sx={{ padding: '0 0 0 2vw' }}>
-
-                    </Typography>
+                    <Box>
+                        <Image src='/image/regeno/reg.gif' alt='Search and Register' width={1000} height={300} style={{ pointerEvents: 'none', userSelect: 'none' }} />
+                    </Box>
                 </Box>
             </Box>
         )
